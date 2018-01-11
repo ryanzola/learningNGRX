2476480b30ad0cbbb73083d9cc8b1038879daf58
ryanzola <ryanzola@me.com>
2018-01-11 01:10:34 -0500

    reducer and actions

--- src/app/pizza/pizza.reducer.ts
+++ src/app/pizza/pizza.reducer.ts
@@ -1,51 +1,54 @@
-import * as actions from './pizza.actions';
-import { EntityState, createEntityAdapter } from '@ngrx/entity';
-import { createFeatureSelector } from '@ngrx/store/src/selector';
+import * as actions from "./pizza.actions";
+import { EntityState, createEntityAdapter } from "@ngrx/entity";
+import { createFeatureSelector } from "@ngrx/store/src/selector";
 
 export interface Pizza {
-    id: string,
-    size: string
+  id: string;
+  size: string;
 }
 
 export const pizzaAdapter = createEntityAdapter<Pizza>();
-export interface State extends EntityState<Pizza> { }
+export interface State extends EntityState<Pizza> {}
 
 const defaultPizza = {
-    ids: ['123'],
-    entities: {
-        '123': {
-            id: '123',
-            size: 'small'
-        }
+  ids: ["123"],
+  entities: {
+    "123": {
+      id: "123",
+      size: "small"
     }
-}
+  }
+};
 
 export const initialState: State = pizzaAdapter.getInitialState(defaultPizza);
 
 export function pizzaReducer(
-    state: State = initialState,
-    action: actions.PizzaActions) {
+  state: State = initialState,
+  action: actions.PizzaActions
+) {
+  switch (action.type) {
+    case actions.CREATE:
+      return pizzaAdapter.addOne(action.pizza, state);
+    case actions.UPDATE:
+      return pizzaAdapter.updateOne(
+        {
+          id: action.id,
+          changes: action.changes
+        },
+        state
+      );
+    case actions.DELETE:
+      return pizzaAdapter.removeOne(action.id, state);
+    default:
+      return state;
+  }
+}
 
-        switch(action.type) {
-            case actions.CREATE:
-                return pizzaAdapter.addOne(action.pizza, state);
-            case actions.UPDATE:
-                return pizzaAdapter.updateOne({
-                    id: action.id,
-                    changes: action.changes
-                }, state);
-            case actions.DELETE:
-                return pizzaAdapter.removeOne(action.id, state);
-            default:
-                return state
-        }
-    }
-
-export const getPizzaState = createFeatureSelector<State>('pizza');
+export const getPizzaState = createFeatureSelector<State>("pizza");
 
 export const {
-    selectIds,
-    selectEntities,
-    selectAll,
-    selectTotal,
+  selectIds,
+  selectEntities,
+  selectAll,
+  selectTotal
 } = pizzaAdapter.getSelectors(getPizzaState);
